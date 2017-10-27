d111940d48aafc0bed06272db8293e35d832d491
Chris Johnson <cknightjohnson@gmail.com>
2017-10-27 10:33:48 -0700

    remove random number and replace with loop that counts up to user num

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,23 @@
 function beepBoop(userNum){
-  var noGood = "That's not a good number, Jerry!";
-  if (userNum >= 0) {
-    userNum = Math.ceil(Math.random() * userNum);
-    userNum = String(userNum);
+  var noGood = "That's not a good number, Jim!";
+  if (userNum > 0) {
+    var returnArray = [];
     var regexOne = /1/g;
     var regexZero = /0/g;
-    if (regexOne.test(userNum)) {
-      return "Boop!";
-    } else {
-      return userNum;
-    }
-  }
+    for (var i = 0; i < userNum; i++) {
+      var iString = String(i); // get string version of i for regex tests
+      if (!(i % 3)) {
+        returnArray.push("I'm sorry, Dave. I'm afraid I can't do that.")
+      } else if (regexOne.test(iString)) {
+        returnArray.push("Boop!");
+      } else if (regexZero.test(iString)) {
+        returnArray.push("Beep!");
+      } else {
+        returnArray.push(i);
+      }
+    } // end for loop
+    return returnArray;
+  } // end if number
   return noGood;
 };
 
