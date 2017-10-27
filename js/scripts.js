74a71429c4e4ec97d886841bf721d6ceb22c80c9
Chris Johnson <cknightjohnson@gmail.com>
2017-10-27 11:05:31 -0700

    add <ul> and insert each array element as new <li>

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,10 @@
 function beepBoop(userNum){
   var noGood = "That's not a good number, Jim!";
-  if (userNum > 0) {
+  if (userNum >= 0) {
     var returnArray = [];
     var regexOne = /1/g;
     var regexZero = /0/g;
-    for (var i = 0; i < userNum; i++) {
+    for (var i = 1; i <= userNum; i++) {
       var iString = String(i); // get string version of i for regex tests
       if (!(i % 3)) {
         returnArray.push("I'm sorry, Dave. I'm afraid I can't do that.")
@@ -16,6 +16,8 @@ function beepBoop(userNum){
         returnArray.push(i);
       }
     } // end for loop
+    // returnArray = returnArray.join(" ");
+    console.log(returnArray);
     return returnArray;
   } // end if number
   return noGood;
@@ -25,6 +27,10 @@ $(document).ready(function() {
   $('#user-form').submit(function(event) {
     event.preventDefault();
     var userNum = $('#user-input').val();
-    $('#display-div').text(beepBoop(userNum));
-  });
-});
+    var returnArray = beepBoop(userNum);
+    console.log(returnArray);
+    returnArray.forEach(function(each) {
+      $('#display-ul').append('<li>' + each + '</li>');
+    }); // end for each
+  }); // end form submit
+}); // end doc ready
